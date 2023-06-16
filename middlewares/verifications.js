c561e75b2d60e4b543004beff15bc0e2b5f92dcf
elgusa095 <elgusa095@gmail.com>
2023-06-16 02:21:25 -0500

    debug

--- middlewares/verifications.js
+++ middlewares/verifications.js
@@ -30,7 +30,7 @@ const verifies = (req, res, next) => {
       } else {
         console.log('IP Limpia');
 
-        res.header('Access-Control-Allow-Origin', ORIGIN_URL);
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-Token, X-Frame-Options, X-Content-Type-Options, X-XSS-Protection');
 
